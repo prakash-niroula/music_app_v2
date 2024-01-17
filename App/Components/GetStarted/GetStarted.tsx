5a50611f74f9070a6a3835dc693e124349a2d639
Prakash Niroula <prakashpradipbrothers@gmail.com>
2024-01-17 18:44:56 +0545

    size changes

--- App/Components/GetStarted/GetStarted.tsx
+++ App/Components/GetStarted/GetStarted.tsx
@@ -11,19 +11,18 @@ export const GetStarted: FC = () => {
 
   return (
     <View
-      w='100%' h='100%' fd='column' jc='center' px={scaleX(25)} mt={scaleY(30)}
+      w='100%' h='100%' fd='column' jc='center' px={scaleX(25)} mt={scaleX(30)}
     >
       <View>
         <Text
-          
           fos={scaleX(40)} ls={-1} fow='bold' ff={'$heading'} col={Colors.black_main}
         >
           Get Started.
         </Text>
       </View>
-      <View mt={scaleY(30)}>
+      <View mt={scaleX(30)}>
         <Button
-          w='100%' br={5} h={scaleY(50)} bg={Colors.grey_rect}
+          w='100%' br={5} h={scaleX(50)} bg={Colors.grey_rect}
         >
           <ButtonIcon>
             <SpotifySvg color={Colors.green_spotify} width={scaleX(24)} height={scaleX(24)} />
@@ -35,7 +34,7 @@ export const GetStarted: FC = () => {
           </ButtonText>
         </Button>
         <Button
-          w='100%' br={5} h={scaleY(50)} mt={scaleY(10)} bg={Colors.grey_rect}
+          w='100%' br={5} h={scaleX(50)} mt={scaleX(10)} bg={Colors.grey_rect}
         >
           <ButtonIcon>
             <YoutubeSvg color={Colors.red_youtube} width={scaleX(25)} height={scaleX(25)} />
@@ -47,14 +46,14 @@ export const GetStarted: FC = () => {
           </ButtonText>
         </Button>
         <View
-          fd='row' ai='center' mt={scaleY(8)}
+          fd='row' ai='center' mt={scaleX(8)}
         >
           <View fg={1} h={1} bg={Colors.grey_tertiary} mt={2}></View>
           <Text px={5} ls={-.5} col={Colors.grey_secondary}>or</Text>
           <View fg={1} h={1} bg={Colors.grey_tertiary} mt={2}></View>
         </View>
         <Button
-          w='100%' br={5} h={scaleY(50)} mt={scaleY(8)} bg={Colors.blue_main}
+          w='100%' br={5} h={scaleX(50)} mt={scaleX(8)} bg={Colors.blue_main}
         >
           <ButtonText
             ff='$body' fos={scaleX(16)} ls={-.5} col='white'
